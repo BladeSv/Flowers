64028e4fc65cfa4541c3788faee6995134e5c75d
BladeSv <39922349+BladeSv@users.noreply.github.com>
2018-06-06 19:31:31 +0300

    Delete DateSection.java

--- src/by/htp/flowers/emtty/DateSection.java
+++
@@ -1,9 +0,0 @@
-package by.htp.flowers.emtty;
-
-public class DateSection extends Flower{
-private int date;
-public DateSection( int date) {
-	// TODO Auto-generated constructor stub
-	this.date=date;
-}
-}
